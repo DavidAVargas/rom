2390d1b681c3677754d22e90eef55ebcc0068b60
David A. Vargas <davidavargas.dev@gmail.com>
2025-08-01 18:52:26 -0500

    Update global styles to include text color and replace header with Hero component in the main page layout.

---
+++ src/components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Container from './Container'
+
+export default function Hero() {
+  return (
+    <section className="flex flex-col items-center justify-center">
+      <Container>
+        <h1>Hero</h1>
+      </Container>
+    </section>
+  )
+}
